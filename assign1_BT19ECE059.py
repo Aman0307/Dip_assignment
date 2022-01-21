24697d84ff5fb7a59bac525f8a7922cce6db9c82
Aman0307 <59815207+Aman0307@users.noreply.github.com>
2022-01-21 13:20:13 +0530

    File to convert rgb  image to grayscale image

---
+++ assign1_BT19ECE059.py
@@ -0,0 +1,25 @@
+# Assignment 1 : Convert image to grayscale without built-in function
+
+import cv2
+import numpy as np
+
+img = cv2.imread("C:\\Users\\amans\\Downloads\\download.jfif")
+print('Image Shape',np.shape(img))
+print(type(img[0][0][0]))
+
+(row, col) = img.shape[0:2]
+cv2.imshow('Original', img)
+cv2.waitKey(0)
+gray = np.zeros([row, col], dtype=np.uint8)
+for i in range(0,row):
+    for j in range(0, col):
+        s = 0
+        for k in range(0,3):
+            s += img[i][j][k]
+        gray[i][j] = s/3
+
+cv2.imshow('Grayscale', gray)
+cv2.waitKey(0)        
+cv2.imwrite('vj_gray.jpg',gray)
+#cv2.waitKey(0)
+cv2.destroyAllWindows()
